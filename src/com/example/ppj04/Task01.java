c36d87daf8e902e7691b899522e0b1a60185f06a
dtkac <dtkach1994@gmail.com>
2023-03-20 13:32:08 +0200

    Task 01 fix

--- src/com/example/ppj04/Task01.java
+++ src/com/example/ppj04/Task01.java
@@ -2,14 +2,14 @@ package com.example.ppj04;
 
 public class Task01 {
     public static void main(String[] args) {
-        // Цілі числа
+        // Whole numbers
         int a = 13;
         int b = 3;
-        int sum = a + b; // Додавання
-        int diff = a - b; // Віднімання
-        int product = a * b; // Множення
-        int quotient = a / b; // Ділення
-        int remainder = a % b; // Остача від ділення
+        int sum = a + b; // Addition
+        int diff = a - b; // Subtraction
+        int product = a * b; // Multiplication
+        int quotient = a / b; // Division
+        int remainder = a % b; // Remainder (Modulus) of division
 
         System.out.println("Integer arithmetic:");
         System.out.println("Sum: " + sum);
@@ -18,14 +18,14 @@ public class Task01 {
         System.out.println("Quotient: " + quotient);
         System.out.println("Remainder: " + remainder);
 
-        // Дійсні числа
+        //Real numbers
         double x = 13.0;
         double y = 3.0;
-        double sum2 = x + y; // Додавання
-        double diff2 = x - y; // Віднімання
-        double product2 = x * y; // Множення
-        double quotient2 = x / y; // Ділення
-        double remainder2 = x % y; // Остача від ділення (не підтримується)
+        double sum2 = x + y; // Addition
+        double diff2 = x - y; // Subtraction
+        double product2 = x * y; // Multiplication
+        double quotient2 = x / y; // Division
+        double remainder2 = x % y; // Remainder (Modulus) of division
 
         System.out.println("Arithmetic of real numbers:");
         System.out.println("Sum: " + sum2);
@@ -34,14 +34,14 @@ public class Task01 {
         System.out.println("Quotient: " + quotient2);
         System.out.println("Remainder: " + remainder2); // Output NaN (not a number)
 
-        // Символьні значення
+        // Symbolic values
         char c1 = 'a';
         char c2 = 'b';
-        int sum3 = c1 + c2; // Додавання
-        int diff3 = c2 - c1; // Віднімання
-        int product3 = c1 * c2; // Множення
-        int quotient3 = c2 / c1; // Ділення
-        int remainder3 = c2 % c1; // Остача від ділення
+        int sum3 = c1 + c2; // Addition
+        int diff3 = c2 - c1; // Subtraction
+        int product3 = c1 * c2; // Multiplication
+        int quotient3 = c2 / c1; // Division
+        int remainder3 = c2 % c1; // Remainder (Modulus) of division
 
         System.out.println("Character arithmetic:");
         System.out.println("Sum: " + sum3);
