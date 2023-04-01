dc32c46ef41af02ba9fe96a004e09cbd31357a1d
dtkac <dtkach1994@gmail.com>
2023-04-01 11:55:54 +0300

    ppj05 task 5.1 comments add

--- src/com/example/ppj05/Task01.java
+++ src/com/example/ppj05/Task01.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Task01 {
 
     public static void main(String[] args) {
+        // Input three numbers from the keyboard.
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input number a: ");
         int a = scanner.nextInt();
@@ -11,17 +12,24 @@ public class Task01 {
         System.out.print("Input number c: ");
         int c = scanner.nextInt();
         scanner.close();
+
+        // Finding the average value.
         int middleValue = findMiddleValue(a, b, c);
+
+        // Printing the average value to the screen.
         System.out.println("Average value: " + middleValue);
     }
 
     public static int findMiddleValue(int a, int b, int c) {
-        if ((a >= b && a <= c) || (a >= c && a <= b)) {
+        // If a is between b and c, a is the middle value
+        if ((a >= b && a <= c) || (a >= c && a <= b)) {    //&& - means and, stop forgetting!! (comment for me)
             return a;
         }
-        else if ((b >= a && b <= c) || (b >= c && b <= a)) {
+        // If b is between a and c, b is the middle value
+        else if ((b >= a && b <= c) || (b >= c && b <= a)) {  // || - reminder it s mean OR
             return b;
         }
+        // Otherwise, c is the middle value
         else {
             return c;
         }
