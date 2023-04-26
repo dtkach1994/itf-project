42639228b53e24bf0cd6cfb456f8fcc6ff1382f8
dtkac <dtkach1994@gmail.com>
2023-04-26 21:49:08 +0300

    PPJ5 revert changes

--- src/com/example/ppj04/Task04.java
+++ src/com/example/ppj04/Task04.java
@@ -1,32 +1,16 @@
 package com.example.ppj04;
 
 public class Task04 {
-    /*
-        Lets say your student number (without the s) represents a hexadecimal number. Convert by hand
-        (without using a converter) and print out this number in base2.
-     */
     public static void main(String[] args) {
-//        int hexNumber = 0x3A5F;
-//        int number1 = 19;
-//        int number2 = 0x4A;
-//        int number3 = 0x11;
-//
-       int number1 = 19;
-       int number2 = 0b1+1;
-       int number3 = 0b100;
-
-
+        String hexNumber = "3A5F";
         String binaryNumber = "";
-//        System.out.println(hexNumber);
-        System.out.println(number1);
-        System.out.println(number2);
-        System.out.println(number3);
-//        for (int i = 0; i < hexNumber.length(); i++) {
-//            char hexDigit = hexNumber.charAt(i);
-//            String binaryDigit = Integer.toBinaryString(Integer.parseInt(String.valueOf(hexDigit), 16));
-//            binaryNumber += String.format("%04d", Integer.parseInt(binaryDigit));
-//        }
-//
-//        System.out.println(binaryNumber); // Output: 0011101001011111
+
+        for (int i = 0; i < hexNumber.length(); i++) {
+            char hexDigit = hexNumber.charAt(i);
+            String binaryDigit = Integer.toBinaryString(Integer.parseInt(String.valueOf(hexDigit), 16));
+            binaryNumber += String.format("%04d", Integer.parseInt(binaryDigit));
+        }
+
+        System.out.println(binaryNumber); // Output: 0011101001011111
     }
 }
